07f6695bcb02d52298edd49a33e2ed88b12536e8
DhaniSanJose <sanjose.dhani@gmail.com>
2024-05-02 19:25:16 +0800

    Working Login and Logout with Display Data

--- ipat2c/dcrm/urls.py
+++ ipat2c/dcrm/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register/', views.register, name='registration')
+    path('register/', views.register, name='registration'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- ipat2c/dcrm/views.py
+++ ipat2c/dcrm/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .models import Record
 # Create your views here.
 
 def home(request):
     #DO SOMETHING
 
-    return render(request, 'home.html')
+    records = Record.objects.all()
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+             messages.success(request, "There was an Error Logging In, Please Try Again")
+             return redirect('home')
+    else:
+
+        return render(request, 'home.html', {'records':records})
+
+
+
+
 
 def register(request):
 
     return render(request, 'register.html')
+
+
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out!")
+    return redirect('home')
